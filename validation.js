0274d4439538b372f35730362a7e7967995c7a74
omern <nivom@post.bgu.ac.il>
2021-04-24 13:46:39 +0300

    LoginCheck Middle and userexists reg

--- validation.js
+++ validation.js
@@ -1,9 +1,20 @@
+var usersDict = {}
+usersDict["k"] = ["k","Erez shalom","er@sh","2020-12-12"]
+
+
 $(function(){
 
+  // Method that checks if user exists in registration
+  $.validator.addMethod('userExists',function(value,element){
+    return !(value in usersDict);
+  })
+
+  // Check that the password has at least 6 chars
     $.validator.addMethod('passwordLength',function(value,element){
         return this.optional(element) || value.length >=6;
     })
 
+    // Check that there is one char and one digit in password
     $.validator.addMethod('passwordDigitChar',function(value,element){
         return this.optional(element) || /\d/.test(value)
         && /[a-z]/i.test(value);
@@ -32,7 +43,8 @@ $(function(){
     var validator= $("#RegisterForm").validate({
         rules: {
           Username:{
-            required: true
+            required: true,
+            userExists: true
           },
           Password: {
             required: true,
@@ -49,8 +61,9 @@ $(function(){
           }
         },            
         messages: {
-        UserName:{
-            required:"Please enter user name"
+        Username:{
+            required:"Please enter user name",
+            userExists:"The user name exists. try another one"
         },
         Password: {
             required: "Please enter password",
@@ -70,9 +83,53 @@ $(function(){
             label.insertAfter(element);
           },
           wrapper: 'span'
-    }
-})
- 
-         
+        
+      }
+    })   
 
 })
+
+function AddUser(){
+  alert("blablablaaaaa");
+  if($('#RegisterForm').valid()){
+    alert("if is passed");
+    let userName=$("#Username").value;
+    let password=$("#Password").value;
+    let fullName=$("#FullName").value;
+    let email=$("#email").value;
+    let date=$("#date").value;
+    usersDict[userName]=[password, fullName, email, birthDate];
+  }
+  // else{
+  //   resetRegisterForm();
+  // }
+  console.log(usersDict);
+}
+
+// is user exists then move him to the Settings page in Login
+function CheckIfUserExists(){
+  // var userEntered = document.getElementById("LoginUsername").value();
+  // alert(userEntered);
+  // var passwordEntered = document.getElementById("LoginPassword").value();
+  // alert(passwordEntered);
+  var userEntered = $("#LoginUsername").val()
+  var passwo
+  alert($("#LoginUsername").val());
+  if($("#LoginUsername").val() in usersDict){
+    alert("user in dict")
+    alert($("#LoginPassword").val());
+   
+    if( usersDict[userEntered][0].localeCompare($("#LoginPassword").val()) === 0){
+     alert("pass in dict");
+      // Do Show Page
+     showPage('Settings');
+    }
+    else {
+      alert("WrongPassword");
+    }
+  }
+  else{
+    alert("the user name doesn't exists");
+  }
+
+}
